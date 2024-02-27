9080eb736528e6eb6804544479952988a49f1db7
shubham <shubhamdixit863@gmail.com>
2024-02-27 11:20:09 +0530

    fixes
    Signed-off-by: shubham <shubhamdixit863@gmail.com>

--- internal/git/gitsync_processor_test.go
+++ internal/git/gitsync_processor_test.go
@@ -38,9 +38,10 @@ import (
 	"k8s.io/client-go/kubernetes/scheme"
 	k8sClient "sigs.k8s.io/controller-runtime/pkg/client"
 
+	mocksClient "github.com/numaproj-labs/numaplane/internal/kubernetes/mocks"
+
 	"github.com/numaproj-labs/numaplane/api/v1alpha1"
 	controllerconfig "github.com/numaproj-labs/numaplane/internal/controller/config"
-	mocksClient "github.com/numaproj-labs/numaplane/internal/kubernetes/mocks"
 	gitshared "github.com/numaproj-labs/numaplane/internal/shared/git"
 )
 
@@ -134,6 +135,12 @@ func TestMain(m *testing.M) {
 	if err := pool.Retry(func() error {
 		// Implement checks for both the Apache server and SSH service here
 
+		// Check SSH service availability
+		_, err = net.Dial("tcp", "localhost:2222") // Adjust port if necessary
+		if err != nil {
+			return fmt.Errorf("SSH service not yet ready")
+		}
+
 		// Check Apache server availability
 		client := &http.Client{}
 		req, err := http.NewRequest("GET", "http://localhost:8080", nil)
@@ -151,12 +158,6 @@ func TestMain(m *testing.M) {
 				log.Println(err)
 			}
 		}(resp.Body)
-
-		// Check SSH service availability
-		_, err = net.Dial("tcp", "localhost:2222") // Adjust port if necessary
-		if err != nil {
-			return fmt.Errorf("SSH service not yet ready")
-		}
 
 		// If both checks pass, return nil to indicate success
 		return nil
